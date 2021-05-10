7ebdf19713683527c73933a07750c44a3c07d611
Alejandro Gómez <alejandro_gomez94@hotmail.com>
2021-05-10 11:17:03 -0500

    Se realiza prueba creando el primer modelo producto

---
+++ pruebatecnica/pruebatecnica/Data/pruebatecnicaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using pruebatecnica.Models;
+
+namespace pruebatecnica.Data
+{
+    public class pruebatecnicaContext : DbContext
+    {
+        public pruebatecnicaContext (DbContextOptions<pruebatecnicaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<pruebatecnica.Models.Producto> Producto { get; set; }
+    }
+}
